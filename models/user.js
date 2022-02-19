98cd9d661b4d3f63ef25f5cc9a135e51a9d1d21c
Vinit Parekh <77158947+vinitsoni17@users.noreply.github.com>
2022-02-19 09:52:35 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 const { Schema, model} = require("mongoose");
 
 const nameSchema = new Schema({
-    userName: {type: String, unique: true},
-    crushName: {type: String, unique: true}
+    userName: {type: String, unique: true, required: true},
+    crushName: {type: String, unique: true, required: true}
 })
 
 const coupleModel = new model("coupleData", nameSchema)
